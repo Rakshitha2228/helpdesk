4c4abe8b076ade659adf11581bf30a2e30aedec3
RAKSHITHA M <rakshithamani28@gmail.com>
2025-07-03 20:03:10 +0530

    Updated LogoutServlet.java

--- src/main/java/servlet/LogoutServlet.java
+++ src/main/java/servlet/LogoutServlet.java
@@ -6,16 +6,11 @@ import javax.servlet.http.*;
 
 @WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        // Invalidate the session to log out
-        HttpSession session = request.getSession(false); // false = don't create new if none
+        HttpSession session = request.getSession(false); 
         if (session != null) {
             session.invalidate();
         }
-
-        // Redirect to login page
         response.sendRedirect("login.html");
     }
 }
